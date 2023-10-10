7a08d780b0188fa81cf40329d5529a4ddb3fd3ff
snoozie-v <gettingsleepyz@gmail.com>
2023-10-09 23:24:24 -0700

    extending date range

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const connex = new Connex({
 const startDateTimeString = "10/1/23 12:00 AM PST";
 const startTimeStamp = Date.parse(startDateTimeString) / 1000; 
 
-const endDateTimeString = "10/9/23 11:59 PM PST";
+const endDateTimeString = "10/15/23 11:59 PM PST";
 const endTimeStamp = Date.parse(endDateTimeString) / 1000;
 
 export default function App() {
